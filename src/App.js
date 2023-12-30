f1a2719859e4e1328f86d4d3018e69929dd1061b
ashleybrandoninforcer <ashley.brandon@inforcer.com>
2023-12-30 20:40:30 +0000

    GA page views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { useState } from 'react'
 import { Login, Logout, Nav, Home, About, News } from './Components'
 import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 //Styles
 import './App.css'
+//Google Analytics
+import ReactGA from 'react-ga4'
 
+ReactGA.initialize("G-5PKG573GLN")
+
+// Send initial page load to Google Analytics
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
+
+const history = createBrowserHistory()
+
+history.listen(location => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: location.pathname,
+  })
+})
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(
@@ -22,20 +38,22 @@ const App = () => {
 
   return (
     <div>
-      {!isLoggedIn ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        <>
-          <Nav />
-          <Routes>
-            <Route path="*" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/news" element={<News />} />
-          </Routes>
-          <Logout onLogout={handleLogout} />
-        </>
-      )}
+      <Router history={history}>
+        {!isLoggedIn ? (
+          <Login onLogin={handleLogin} />
+        ) : (
+          <>
+            <Nav />
+            <Routes>
+              <Route path="*" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+            <Logout onLogout={handleLogout} />
+          </>
+        )}
+      </Router>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-//Google Analytics
-import ReactGA from 'react-ga4'
-ReactGA.initialize("G-5PKG573GLN")
-
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-  title: "test"
-})
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
