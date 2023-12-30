cb775adfb435a7c114cd4bd9ac6f675a820cd9b9
ashleybrandoninforcer <ashley.brandon@inforcer.com>
2023-12-30 20:56:14 +0000

    Page views

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import { useState } from 'react'
 import { Login, Logout, Nav, Home, About, News } from './Components'
 import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import { UsePageViews } from './UsePageViews'
 //Styles
 import './App.css'
 //Google Analytics
 import ReactGA from 'react-ga4'
 
 ReactGA.initialize("G-5PKG573GLN")
-
-// Send initial page load to Google Analytics
-ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
-
-const history = createBrowserHistory()
-
-history.listen(location => {
-  ReactGA.send({
-    hitType: "pageview",
-    page: location.pathname,
-  })
-})
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(
@@ -36,9 +24,11 @@ const App = () => {
   localStorage.removeItem('BasicLoginAuth')
  }
 
+ UsePageViews()
+
   return (
     <div>
-      <Router history={history}>
+      <Router>
         {!isLoggedIn ? (
           <Login onLogin={handleLogin} />
         ) : (
